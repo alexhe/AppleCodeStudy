dc5fea76ae4e54c23675743e493db0b322f5dcae
alexhe <alexhe2011@gmail.com>
2020-06-03 15:37:10 +0800

    apple open sourcecode download shell

---
+++ macos-10153/src.sh
@@ -0,0 +1,196 @@
+# wget https://opensource.apple.com/tarballs/AppleFileSystemDriver/AppleFileSystemDriver-23.tar.gz
+# wget https://opensource.apple.com/tarballs/AppleRAID/AppleRAID-16.tar.gz
+# wget https://opensource.apple.com/tarballs/AppleUSBIrDA/AppleUSBIrDA-147.tar.gz
+# wget https://opensource.apple.com/tarballs/AvailabilityVersions/AvailabilityVersions-45.5.tar.gz
+# wget https://opensource.apple.com/tarballs/BerkeleyDB/BerkeleyDB-23.tar.gz
+# wget https://opensource.apple.com/tarballs/BootCache/BootCache-151.tar.gz
+# wget https://opensource.apple.com/tarballs/CPAN/CPAN-73.tar.gz
+# wget https://opensource.apple.com/tarballs/Chess/Chess-347.tar.gz
+# wget https://opensource.apple.com/tarballs/CommonCrypto/CommonCrypto-60165.tar.gz
+# wget https://opensource.apple.com/tarballs/CrackLib/CrackLib-37765.tar.gz
+# wget https://opensource.apple.com/tarballs/Csu/Csu-88.tar.gz
+# wget https://opensource.apple.com/tarballs/DiskArbitration/DiskArbitration-316.0.5.tar.gz
+# wget https://opensource.apple.com/tarballs/Heimdal/Heimdal-564.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/ICU/ICU-64252.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/IOATAFamily/IOATAFamily-256.tar.gz
+# wget https://opensource.apple.com/tarballs/IOATAPIProtocolTransport/IOATAPIProtocolTransport-351.tar.gz
+# wget https://opensource.apple.com/tarballs/IOAudioFamily/IOAudioFamily-300.2.tar.gz
+# wget https://opensource.apple.com/tarballs/IOBDStorageFamily/IOBDStorageFamily-19.tar.gz
+# wget https://opensource.apple.com/tarballs/IOCDStorageFamily/IOCDStorageFamily-58.tar.gz
+# wget https://opensource.apple.com/tarballs/IODVDStorageFamily/IODVDStorageFamily-42.tar.gz
+# wget https://opensource.apple.com/tarballs/IOFireWireAVC/IOFireWireAVC-428.tar.gz
+# wget https://opensource.apple.com/tarballs/IOFireWireFamily/IOFireWireFamily-475.tar.gz
+# wget https://opensource.apple.com/tarballs/IOFireWireIP/IOFireWireIP-230.tar.gz
+# wget https://opensource.apple.com/tarballs/IOFireWireSBP2/IOFireWireSBP2-434.tar.gz
+# wget https://opensource.apple.com/tarballs/IOFireWireSerialBusProtocolTransport/IOFireWireSerialBusProtocolTransport-257.tar.gz
+# wget https://opensource.apple.com/tarballs/IOGraphics/IOGraphics-569.4.tar.gz
+# wget https://opensource.apple.com/tarballs/IOHIDFamily/IOHIDFamily-1446.80.2.tar.gz
+# wget https://opensource.apple.com/tarballs/IOKitTools/IOKitTools-112.tar.gz
+# wget https://opensource.apple.com/tarballs/IOKitUser/IOKitUser-1726.80.1.tar.gz
+# wget https://opensource.apple.com/tarballs/IONetworkingFamily/IONetworkingFamily-139.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/IOPCIFamily/IOPCIFamily-370.81.1.tar.gz
+# wget https://opensource.apple.com/tarballs/IOSCSIParallelFamily/IOSCSIParallelFamily-301.tar.gz
+# wget https://opensource.apple.com/tarballs/IOStorageFamily/IOStorageFamily-238.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/IOUSBMassStorageClass/IOUSBMassStorageClass-408.tar.gz
+# wget https://opensource.apple.com/tarballs/JavaScriptCore/JavaScriptCore-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/KerberosHelper/KerberosHelper-159.tar.gz
+# wget https://opensource.apple.com/tarballs/Libc/Libc-1353.60.8.tar.gz
+# wget https://opensource.apple.com/tarballs/Libinfo/Libinfo-538.tar.gz
+# wget https://opensource.apple.com/tarballs/Libnotify/Libnotify-241.tar.gz
+# wget https://opensource.apple.com/tarballs/Librpcsvc/Librpcsvc-26.tar.gz
+# wget https://opensource.apple.com/tarballs/Libsystem/Libsystem-1281.tar.gz
+# wget https://opensource.apple.com/tarballs/Liby/Liby-20.tar.gz
+# wget https://opensource.apple.com/tarballs/MITKerberosShim/MITKerberosShim-77.tar.gz
+# wget https://opensource.apple.com/tarballs/NFS/NFS-136.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/OpenAL/OpenAL-64.tar.gz
+# wget https://opensource.apple.com/tarballs/OpenLDAP/OpenLDAP-528.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/OpenPAM/OpenPAM-25.tar.gz
+# wget https://opensource.apple.com/tarballs/OpenSSH/OpenSSH-235.tar.gz
+# wget https://opensource.apple.com/tarballs/OpenSSL098/OpenSSL098-80.tar.gz
+# wget https://opensource.apple.com/tarballs/PostgreSQL/PostgreSQL-207.14.tar.gz
+# wget https://opensource.apple.com/tarballs/PowerManagement/PowerManagement-956.80.2.tar.gz
+# wget https://opensource.apple.com/tarballs/RubyGems/RubyGems-31.tar.gz
+# wget https://opensource.apple.com/tarballs/Security/Security-59306.80.4.tar.gz
+# wget https://opensource.apple.com/tarballs/SecurityTokend/SecurityTokend-55113.tar.gz
+# wget https://opensource.apple.com/tarballs/SmartcardCCID/SmartcardCCID-55018.0.2.tar.gz
+# wget https://opensource.apple.com/tarballs/TimeZoneData/TimeZoneData-92.tar.gz
+# wget https://opensource.apple.com/tarballs/UserNotification/UserNotification-30.tar.gz
+# wget https://opensource.apple.com/tarballs/WTF/WTF-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/WebCore/WebCore-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/WebInspectorUI/WebInspectorUI-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/WebKit/WebKit-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/WebKit2/WebKit2-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/adv_cmds/adv_cmds-174.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/apache/apache-852.tar.gz
+# wget https://opensource.apple.com/tarballs/apache_mod_hfs_apple/apache_mod_hfs_apple-16.tar.gz
+# wget https://opensource.apple.com/tarballs/apache_mod_perl/apache_mod_perl-112.tar.gz
+# wget https://opensource.apple.com/tarballs/apache_mod_php/apache_mod_php-148.tar.gz
+# wget https://opensource.apple.com/tarballs/apr/apr-42.tar.gz
+# wget https://opensource.apple.com/tarballs/architecture/architecture-279.tar.gz
+# wget https://opensource.apple.com/tarballs/autofs/autofs-281.80.1.tar.gz
+# wget https://opensource.apple.com/tarballs/awk/awk-24.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/bash/bash-118.40.2.tar.gz
+# wget https://opensource.apple.com/tarballs/basic_cmds/basic_cmds-55.tar.gz
+# wget https://opensource.apple.com/tarballs/bc/bc-21.tar.gz
+# wget https://opensource.apple.com/tarballs/bind9/bind9-57.4.tar.gz
+# wget https://opensource.apple.com/tarballs/bless/bless-181.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/bmalloc/bmalloc-7608.5.11.tar.gz
+# wget https://opensource.apple.com/tarballs/bootp/bootp-394.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/bootstrap_cmds/bootstrap_cmds-116.tar.gz
+# wget https://opensource.apple.com/tarballs/bzip2/bzip2-44.tar.gz
+# wget https://opensource.apple.com/tarballs/cddafs/cddafs-263.tar.gz
+# wget https://opensource.apple.com/tarballs/configd/configd-1061.80.3.tar.gz
+# wget https://opensource.apple.com/tarballs/copyfile/copyfile-166.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/coreTLS/coreTLS-167.tar.gz
+# wget https://opensource.apple.com/tarballs/cron/cron-39.tar.gz
+# wget https://opensource.apple.com/tarballs/crontabs/crontabs-52.80.1.tar.gz
+# wget https://opensource.apple.com/tarballs/cups/cups-483.2.tar.gz
+# wget https://opensource.apple.com/tarballs/curl/curl-118.tar.gz
+# wget https://opensource.apple.com/tarballs/cxxfilt/cxxfilt-11.2.tar.gz
+# wget https://opensource.apple.com/tarballs/dcerpc/dcerpc-69.tar.gz
+# wget https://opensource.apple.com/tarballs/diskdev_cmds/diskdev_cmds-641.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/disklabel/disklabel-6.tar.gz
+# wget https://opensource.apple.com/tarballs/doc_cmds/doc_cmds-51.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/dtrace/dtrace-338.40.5.tar.gz
+# wget https://opensource.apple.com/tarballs/dyld/dyld-733.8.tar.gz
+# wget https://opensource.apple.com/tarballs/eap8021x/eap8021x-292.40.2.tar.gz
+# wget https://opensource.apple.com/tarballs/expat/expat-19.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/file/file-74.0.2.tar.gz
+# wget https://opensource.apple.com/tarballs/file_cmds/file_cmds-287.40.2.tar.gz
+# wget https://opensource.apple.com/tarballs/files/files-812.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/gnudiff/gnudiff-25.tar.gz
+# wget https://opensource.apple.com/tarballs/gnutar/gnutar-455.tar.gz
+# wget https://opensource.apple.com/tarballs/gpatch/gpatch-4.tar.gz
+# wget https://opensource.apple.com/tarballs/gpt/gpt-18.tar.gz
+# wget https://opensource.apple.com/tarballs/groff/groff-41.tar.gz
+# wget https://opensource.apple.com/tarballs/gssd/gssd-84.tar.gz
+# wget https://opensource.apple.com/tarballs/hfs/hfs-522.0.9.tar.gz
+# wget https://opensource.apple.com/tarballs/hunspell/hunspell-13.tar.gz
+# wget https://opensource.apple.com/tarballs/iodbc/iodbc-42.6.tar.gz
+# wget https://opensource.apple.com/tarballs/ipsec/ipsec-326.81.1.tar.gz
+# wget https://opensource.apple.com/tarballs/kext_tools/kext_tools-623.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/keymgr/keymgr-30.tar.gz
+# wget https://opensource.apple.com/tarballs/ksh/ksh-27.tar.gz
+# wget https://opensource.apple.com/tarballs/less/less-34.tar.gz
+# wget https://opensource.apple.com/tarballs/libarchive/libarchive-72.40.2.tar.gz
+# wget https://opensource.apple.com/tarballs/libauto/libauto-187.tar.gz
+# wget https://opensource.apple.com/tarballs/libclosure/libclosure-74.tar.gz
+# wget https://opensource.apple.com/tarballs/libdispatch/libdispatch-1173.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/libedit/libedit-55.tar.gz
+# wget https://opensource.apple.com/tarballs/libevent/libevent-8.tar.gz
+# wget https://opensource.apple.com/tarballs/libffi/libffi-25.tar.gz
+# wget https://opensource.apple.com/tarballs/libfs/libfs-23.tar.gz
+# wget https://opensource.apple.com/tarballs/libiconv/libiconv-59.tar.gz
+# wget https://opensource.apple.com/tarballs/libmalloc/libmalloc-283.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/libpcap/libpcap-89.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/libplatform/libplatform-220.tar.gz
+# wget https://opensource.apple.com/tarballs/libpthread/libpthread-416.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/libresolv/libresolv-67.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/libsecurity_ldap_dl/libsecurity_ldap_dl-55004.tar.gz
+# wget https://opensource.apple.com/tarballs/libstdcxx/libstdcxx-104.1.tar.gz
+# wget https://opensource.apple.com/tarballs/libtelnet/libtelnet-13.tar.gz
+# wget https://opensource.apple.com/tarballs/libunwind/libunwind-35.4.tar.gz
+# wget https://opensource.apple.com/tarballs/libutil/libutil-57.tar.gz
+# wget https://opensource.apple.com/tarballs/libxml2/libxml2-32.14.tar.gz
+# wget https://opensource.apple.com/tarballs/libxslt/libxslt-16.7.tar.gz
+# wget https://opensource.apple.com/tarballs/llvmCore/llvmCore-3425.0.36.tar.gz
+# wget https://opensource.apple.com/tarballs/lsof/lsof-67.tar.gz
+# wget https://opensource.apple.com/tarballs/mDNSResponder/mDNSResponder-1096.60.2.tar.gz
+# wget https://opensource.apple.com/tarballs/mail_cmds/mail_cmds-35.tar.gz
+# wget https://opensource.apple.com/tarballs/man/man-18.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/misc_cmds/misc_cmds-34.tar.gz
+# wget https://opensource.apple.com/tarballs/modemccl/modemccl-26.tar.gz
+# wget https://opensource.apple.com/tarballs/msdosfs/msdosfs-411.60.1.tar.gz
+# wget https://opensource.apple.com/tarballs/nano/nano-12.tar.gz
+# wget https://opensource.apple.com/tarballs/ncurses/ncurses-57.tar.gz
+# wget https://opensource.apple.com/tarballs/net_snmp/net_snmp-166.tar.gz
+# wget https://opensource.apple.com/tarballs/netcat/netcat-46.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/network_cmds/network_cmds-596.tar.gz
+# wget https://opensource.apple.com/tarballs/ntfs/ntfs-98.1.1.tar.gz
+# wget https://opensource.apple.com/tarballs/ntp/ntp-139.tar.gz
+# wget https://opensource.apple.com/tarballs/objc4/objc4-781.2.tar.gz
+# wget https://opensource.apple.com/tarballs/pam_modules/pam_modules-180.40.1.tar.gz
+# wget https://opensource.apple.com/tarballs/passwordserver_sasl/passwordserver_sasl-213.tar.gz
+# wget https://opensource.apple.com/tarballs/patch_cmds/patch_cmds-17.tar.gz
+# # wget https://opensource.apple.com/tarballs/pcre/pcre-11.tar.gz
+# wget https://opensource.apple.com/tarballs/pdisk/pdisk-9.tar.gz
+# wget https://opensource.apple.com/tarballs/perl/perl-131.tar.gz
+# wget https://opensource.apple.com/tarballs/postfix/postfix-280.tar.gz
+# wget https://opensource.apple.com/tarballs/ppp/ppp-862.tar.gz
+# wget https://opensource.apple.com/tarballs/pyobjc/pyobjc-54.tar.gz
+## wget https://opensource.apple.com/tarballs/python/python-125.tar.gz
+## wget https://opensource.apple.com/tarballs/python_modules/python_modules-58.0.1.tar.gz
+# wget https://opensource.apple.com/tarballs/remote_cmds/remote_cmds-63.tar.gz
+wget https://opensource.apple.com/tarballs/removefile/removefile-48.tar.gz
+wget https://opensource.apple.com/tarballs/rsync/rsync-54.tar.gz
+## wget https://opensource.apple.com/tarballs/ruby/ruby-141.tar.gz
+wget https://opensource.apple.com/tarballs/screen/screen-23.tar.gz
+wget https://opensource.apple.com/tarballs/security_authtrampoline/security_authtrampoline-55106.tar.gz
+wget https://opensource.apple.com/tarballs/security_certificates/security_certificates-55161.80.1.tar.gz
+wget https://opensource.apple.com/tarballs/security_certtool/security_certtool-55119.tar.gz
+wget https://opensource.apple.com/tarballs/security_crlrefresh/security_crlrefresh-55102.tar.gz
+wget https://opensource.apple.com/tarballs/security_ocspd/security_ocspd-55137.tar.gz
+wget https://opensource.apple.com/tarballs/security_systemkeychain/security_systemkeychain-55212.tar.gz
+wget https://opensource.apple.com/tarballs/shell_cmds/shell_cmds-207.40.1.tar.gz
+## wget https://opensource.apple.com/tarballs/subversion/subversion-93.tar.gz
+wget https://opensource.apple.com/tarballs/sudo/sudo-87.80.2.tar.gz
+wget https://opensource.apple.com/tarballs/swig/swig-12.tar.gz
+wget https://opensource.apple.com/tarballs/syslog/syslog-377.60.2.tar.gz
+wget https://opensource.apple.com/tarballs/system_cmds/system_cmds-854.40.2.tar.gz
+wget https://opensource.apple.com/tarballs/system_config/system_config-56.tar.gz
+## wget https://opensource.apple.com/tarballs/tcl/tcl-128.tar.gz
+## wget https://opensource.apple.com/tarballs/tcpdump/tcpdump-90.60.1.tar.gz
+wget https://opensource.apple.com/tarballs/tcsh/tcsh-68.tar.gz
+wget https://opensource.apple.com/tarballs/texinfo/texinfo-19.tar.gz
+wget https://opensource.apple.com/tarballs/text_cmds/text_cmds-101.40.1.tar.gz
+wget https://opensource.apple.com/tarballs/tidy/tidy-17.1.tar.gz
+wget https://opensource.apple.com/tarballs/top/top-125.tar.gz
+wget https://opensource.apple.com/tarballs/usertemplate/usertemplate-108.tar.gz
+wget https://opensource.apple.com/tarballs/uucp/uucp-12.tar.gz
+## wget https://opensource.apple.com/tarballs/vim/vim-78.40.1.tar.gz
+wget https://opensource.apple.com/tarballs/webdavfs/webdavfs-386.tar.gz
+wget https://opensource.apple.com/tarballs/xar/xar-420.tar.gz
+wget https://opensource.apple.com/tarballs/xnu/xnu-6153.81.5.tar.gz
+wget https://opensource.apple.com/tarballs/zip/zip-18.tar.gz
+wget https://opensource.apple.com/tarballs/zlib/zlib-76.tar.gz
+wget https://opensource.apple.com/tarballs/zsh/zsh-84.tar.gz
